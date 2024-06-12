be78b317682af8d4c8798d511d6b033821d13fce
danielbeinat <danielbeinat@outlook.es>
2024-06-12 16:36:11 -0300

    add files

--- src/assets/MyProjects/Projects.js
+++ src/assets/MyProjects/Projects.js
@@ -38,7 +38,7 @@ export const Projects = [
     description: "Desarrollé un convertidor de YouTube a MP3 utilizando React y Vite. La aplicación permite a los usuarios convertir videos de YouTube en archivos de audio MP3 con una interfaz moderna y receptiva. Además, integra la API de YouTube para obtener videos y utiliza i18next para traducir el contenido a varios idiomas.",
     Languages: [react, tailwind],
     url: "https://mp3linker.netlify.app/",
-    github: "https://github.com/danielbeinat/Arcadia.git",
+    github: "https://github.com/danielbeinat/Convert-YouTube-to-MP3",
   },
 
 
