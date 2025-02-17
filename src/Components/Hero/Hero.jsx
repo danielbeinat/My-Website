1c97c65f894605b46a7658901043bd83125e9fb7
danielbeinat <danielbeinat@outlook.es>
2025-02-17 18:10:18 -0300

    add files

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -38,9 +38,9 @@ export const Hero = () => {
 
       <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#06071B]/30 pointer-events-none" />
 
-      {/* <div className="absolute inset-0 opacity-30">
+       <div className="absolute inset-0 opacity-30">
         <div className="absolute inset-0 bg-[radial-gradient(#0CFCA7_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
-      </div> */}
+      </div> 
     </section>
   );
 };
